64c2c4bb7b95680c8cd70cec9182766bbcee435e
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-05 10:24:49 +0200

    ex

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/01.Bac-In-30-Minutes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01.Bac_In_30_Minutes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes += 30;
+            if (minutes > 59)
+            {
+                hours += 1;
+                minutes -= 60;
+            }
+            if (hours > 23)
+            {
+                hours = 0;
+            }
+
+
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
