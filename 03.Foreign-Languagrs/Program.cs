1d4938d43663246878e11d63376f2148de3382a2
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-05 10:27:02 +0200

    ex

---
+++ 03.Foreign-Languagrs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _03.Foreign_Languagrs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string leangueSpeak = Console.ReadLine();
+
+
+            switch (leangueSpeak)
+            {
+                case "England":
+                case "USA":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico": Console.WriteLine("Spanish"); break;
+                default: Console.WriteLine("unknown"); break;
+
+            }
+        }
+    }
+}
