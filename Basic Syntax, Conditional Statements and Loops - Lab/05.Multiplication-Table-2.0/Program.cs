d24686862b30f90f5bf4f2b2ae9b2529d4db2651
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-06 11:58:54 +0200

    ex

---
+++ Basic Syntax, Conditional Statements and Loops - Lab/05.Multiplication-Table-2.0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _05.Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < 10; i++)
+            {
+
+                int currentNum = number * secondNum;
+
+
+                Console.WriteLine($"{number} X {secondNum} = {currentNum}");
+                secondNum++;
+                if (secondNum > 10)
+                {
+                    break;
+                }
+
+            }
+        }
+    }
+}
