7c302877890e97c3dc9e657841f7b66b4477aff4
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-12 21:49:59 +0200

    ex

---
+++ DataTypesAndVariables/07.Triples-of-LatinLetters/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07.Triples_of_LatinLetters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int latin = int.Parse(Console.ReadLine());
+
+            for (char i = 'a'; i < 'a' + latin; i++)
+            {
+
+                for (char k = 'a'; k < 'a' + latin; k++)
+                {
+
+
+                    for (char l = 'a'; l < 'a' + latin; l++)
+                    {
+
+
+                        Console.WriteLine($"{i}{k}{l}");
+                    }
+
+                }
+
+            }
+        }
+    }
+}
