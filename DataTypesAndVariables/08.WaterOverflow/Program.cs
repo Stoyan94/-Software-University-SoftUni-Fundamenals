b7c1c4b081a1bffc77d533e201d1db35174f70c6
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-12 21:51:13 +0200

    ex

---
+++ DataTypesAndVariables/08.WaterOverflow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08.WaterOverflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var timesPour = int.Parse(Console.ReadLine());
+
+            var capacity = 255;
+            var sumCapacity = 0;
+
+            for (int i = 0; i < timesPour; i++)
+            {
+                var waterQuantites = int.Parse(Console.ReadLine());
+
+                sumCapacity += waterQuantites;
+                if (sumCapacity > capacity)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    sumCapacity -= waterQuantites;
+                    continue;
+                }
+
+            }
+            Console.WriteLine(sumCapacity);
+        }
+    }
+}
