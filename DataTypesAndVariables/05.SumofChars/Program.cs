921ed309673d7e40b1194c782b2287efee70a3c9
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-12 21:46:08 +0200

    ex

---
+++ DataTypesAndVariables/05.SumofChars/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _05.SumofChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                char currChar = char.Parse(Console.ReadLine());
+                int numChar = (int)currChar;
+
+                sum += numChar;
+
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
