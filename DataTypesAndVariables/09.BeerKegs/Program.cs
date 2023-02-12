aeeb150a81292fd6ddf7d1c7e6ae9a5a593ad486
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-12 21:53:22 +0200

    ex

---
+++ DataTypesAndVariables/09.BeerKegs/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _09.BeerKegs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //currentVolume 3.14 * radius * radius * heigh
+
+            var numOfKegs = int.Parse(Console.ReadLine());
+            string numOfKeg = String.Empty;
+
+
+            var biggestKeg = 0.0;
+            for (int i = 1; i <= numOfKegs; i++)
+            {
+                var typeBeerKeg = Console.ReadLine();
+                var radius = float.Parse(Console.ReadLine());
+                var height = int.Parse(Console.ReadLine());
+                var currentVolumes = Math.PI * Math.Pow(radius, 2) * height;
+
+
+                if (currentVolumes > biggestKeg)
+                {
+                    biggestKeg = currentVolumes;
+                    numOfKeg = typeBeerKeg;
+                }
+
+            }
+
+            Console.WriteLine(numOfKeg);
+        }
+    }
+}
