5062898e256e7714398252d932e79e0422700766
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-12 21:48:15 +0200

    ex

---
+++ DataTypesAndVariables/06.PrintPartOf-ASCII-Table/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _06.PrintPartOf_ASCII_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int asci = int.Parse(Console.ReadLine());
+            int asci2 = int.Parse(Console.ReadLine());
+
+            for (int i = asci; i <= asci2; i++)
+            {
+
+                char currentChar = (char)i;
+
+                Console.Write($"{currentChar} ");
+            }
+        }
+    }
+}
