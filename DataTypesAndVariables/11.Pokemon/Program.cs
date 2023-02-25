c49e70238a306b61a334c7ad22a3b904a8241b5b
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-26 01:03:57 +0200

    ex

---
+++ DataTypesAndVariables/11.Pokemon/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _11.Pokemon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pokePower = int.Parse(Console.ReadLine());
+            var distanceTrarget = int.Parse(Console.ReadLine());
+            var exhaustion = int.Parse(Console.ReadLine());
+
+            var targetPokeCount = 0;
+            double startTarget = pokePower;
+
+            while (pokePower >= distanceTrarget)
+            {
+                pokePower -= distanceTrarget;
+                targetPokeCount++;
+                if (startTarget / 2 == pokePower && exhaustion != 0)
+                {
+                    pokePower /= exhaustion;
+
+                }
+
+            }
+
+            Console.WriteLine(pokePower);
+            Console.WriteLine(targetPokeCount);
+        }
+    }
+}
