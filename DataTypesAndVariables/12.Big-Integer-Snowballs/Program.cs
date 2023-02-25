db06ad3396a665ac24ad606d4c667d7b00f25327
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-26 01:08:34 +0200

    ex

---
+++ DataTypesAndVariables/12.Big-Integer-Snowballs/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Numerics;
+
+namespace _12.Big_Integer_Snowballs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int snowBallCount = int.Parse(Console.ReadLine());
+
+            int bestSnowballSnow = 0;
+            int bestSnowballTime = 0;
+            int bestSnowballQuality = 0;
+            BigInteger bestSnowballValue = BigInteger.Zero;
+
+            for (int i = 0; i < snowBallCount; i++)
+            {
+
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                int snowballDivede = snowballSnow / snowballTime;
+                BigInteger snowballValue = BigInteger.Pow(snowballDivede, snowballQuality);
+
+                if (snowballValue >= bestSnowballValue)
+                {
+                    bestSnowballSnow = snowballSnow;
+                    bestSnowballTime = snowballTime;
+                    bestSnowballQuality = snowballQuality;
+                    bestSnowballValue = snowballValue;
+                }
+
+
+            }
+            Console.WriteLine($"{bestSnowballSnow} : {bestSnowballTime} = {bestSnowballValue} ({bestSnowballQuality})");
+        }
+    }
+}
