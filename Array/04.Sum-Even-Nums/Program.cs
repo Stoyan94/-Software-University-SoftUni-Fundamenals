404494d0edda53d641adda5c74633667efab866a
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-28 16:30:01 +0200

    ex

---
+++ Array/04.Sum-Even-Nums/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Sum_Even_Nums
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrItem = Console.ReadLine().
+               Split().
+               Select(int.Parse)
+               .ToArray();
+
+            int sum = 0;
+            foreach (var item in arrItem)
+            {
+
+                if (item % 2 == 0)
+                {
+                    sum += item;
+                }
+
+            }
+            Console.WriteLine(String.Join(" ", sum));
+        }
+    }
+}
