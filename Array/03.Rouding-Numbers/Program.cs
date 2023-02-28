b557c7dea7628761febd9fd337ff647127cb1d74
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-28 16:28:09 +0200

    ex

---
+++ Array/03.Rouding-Numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _03.Rouding_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] rawInput = Console.ReadLine().Split();
+            double[] items = new double[rawInput.Length];
+
+            for (int i = 0; i < rawInput.Length; i++)
+            {
+                items[i] = double.Parse(rawInput[i]);
+            }
+
+            // -0.00 - double - print 0.0
+
+            for (int i = 0; i < items.Length; i++)
+            {
+                Console.WriteLine($"{items[i]} => {Math.Round(items[i], MidpointRounding.AwayFromZero)}");
+            }
+        }
+    }
+}
