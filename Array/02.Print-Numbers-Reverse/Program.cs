1d7c6438f7072fb19b3f1c84bd9990bb5758afe4
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-28 16:26:26 +0200

    ex

---
+++ Array/02.Print-Numbers-Reverse/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _02.Print_Numbers_Reverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[] nums = new int[rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                nums[i] = num;
+
+            }
+
+            for (int i = nums.Length - 1; i >= 0; i--)
+            {
+
+                Console.Write($"{nums[i]} ");
+            }
+        }
+    }
+}
