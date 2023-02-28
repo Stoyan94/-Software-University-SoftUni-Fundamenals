e7b4768bd6577f3345b8aeaf87f972f46e9e6d4d
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-02-28 16:31:21 +0200

    ex

---
+++ Array/05.Even-and-Odd-Subtraction/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrItem = Console.ReadLine().
+                Split().
+                Select(int.Parse)
+                .ToArray();
+
+            int sumEven = 0;
+            int sumOdd = 0;
+            foreach (var item in arrItem)
+            {
+
+                if (item % 2 == 0)
+                {
+                    sumEven += item;
+                }
+                else
+                {
+                    sumOdd += item;
+                }
+
+
+            }
+            int finalSum = sumEven - sumOdd;
+            Console.WriteLine(String.Join(" ", finalSum));
+        }
+    }
+}
