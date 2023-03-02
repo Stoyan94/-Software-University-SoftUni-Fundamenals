09bb951ba49882fa5e47898651299a583f497191
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-02 21:59:36 +0200

    ex

---
+++ Array/06.Equal-Arrays/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.Equal_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrItem = Console.ReadLine().
+                Split().
+                Select(int.Parse)
+                .ToArray();
+
+            int[] arrItem2 = Console.ReadLine().
+              Split().
+              Select(int.Parse)
+              .ToArray();
+
+
+            int sum = 0;
+            bool itsSame = true;
+            for (int i = 0; i < arrItem.Length; i++)
+            {
+                sum += arrItem2[i];
+                if (arrItem[i] != arrItem2[i])
+                {
+                    itsSame = false;
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+            }
+
+            if (itsSame)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+
+        }
+    }
+}
