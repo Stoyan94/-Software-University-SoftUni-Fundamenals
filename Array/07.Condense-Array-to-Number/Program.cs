23de9d4e581dc86bc3c7698b8d4f0aa008882916
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-02 22:02:06 +0200

    ex

---
+++ Array/07.Condense-Array-to-Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.Condense_Array_to_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrItem = Console.ReadLine().
+               Split().
+               Select(int.Parse)
+               .ToArray();
+
+            int firstLength = arrItem.Length;
+
+            for (int i = 0; i < firstLength - 1; i++)
+            {
+                int[] condese = new int[arrItem.Length - 1];
+
+                for (int j = 0; j < arrItem.Length - 1; j++)
+                {
+                    condese[j] = arrItem[j] + arrItem[j + 1];
+                }
+
+                arrItem = condese;
+            }
+
+            Console.WriteLine(arrItem[0]);
+        }
+    }
+}
