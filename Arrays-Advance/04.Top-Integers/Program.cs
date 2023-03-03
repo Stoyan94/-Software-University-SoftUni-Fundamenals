987f07eb0cbcf34a12d89c7d28a35e396064ec8f
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-03 18:40:01 +0200

    ex

---
+++ Arrays-Advance/04.Top-Integers/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split(' ').
+                Select(int.Parse).ToArray();
+
+            int[] topInteger = new int[nums.Length];
+            int topIntIndexes = 0;
+            for (int i = 0; i <= nums.Length - 1; i++)
+            {
+                int currNum = nums[i];
+                bool IsTop = true;
+
+                for (int j = i + 1; j <= nums.Length - 1; j++)
+                {
+                    int nextNum = nums[j];
+
+                    if (nextNum >= currNum)
+                    {
+                        IsTop = false;
+                        break;
+                    }
+                }
+
+                if (IsTop)
+                {
+                    topInteger[topIntIndexes] = currNum;
+                    topIntIndexes++;
+                }
+            }
+
+            for (int i = 0; i < topIntIndexes; i++)
+            {
+
+                Console.Write($"{topInteger[i]} ");
+            }
+        }
+    }
+}
