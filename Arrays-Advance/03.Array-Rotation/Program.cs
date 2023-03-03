2f9df78945db915e560d2ca2f37d773a9fa7d914
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-03 18:37:10 +0200

    ex

---
+++ Arrays-Advance/03.Array-Rotation/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Array_Rotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayRotation = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).
+                Select(int.Parse).ToArray();
+            int timesRotation = int.Parse(Console.ReadLine());
+
+
+
+            for (int j = 0; j < timesRotation; j++)
+            {
+                int firstEl = arrayRotation[0];
+                for (int i = 0; i < arrayRotation.Length - 1; i++)
+                {
+                    arrayRotation[i] = arrayRotation[i + 1];
+
+                }
+                arrayRotation[arrayRotation.Length - 1] = firstEl;
+            }
+
+            Console.WriteLine(String.Join(" ", arrayRotation));
+        }
+    }
+}
