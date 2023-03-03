e85f34ab8a853fc8b9f96caa42780698f5c626c4
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-03 18:42:37 +0200

    ex

---
+++ Arrays-Advance/05.Equal-Sum/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.Equal_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split(' ').
+              Select(int.Parse).ToArray();
+            bool isEqual = false;
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                int leftSum = 0;
+                for (int k = 0; k < i; k++)
+                {
+                    leftSum += num[k];
+                }
+                int rightSum = 0;
+                for (int j = num.Length - 1; j > i; j--)
+                {
+                    rightSum += num[j];
+
+                }
+                if (leftSum == rightSum)
+                {
+                    isEqual = true;
+                    Console.WriteLine(i);
+
+                }
+
+            }
+            if (!isEqual)
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
