cba92aed9b022dc61d58dfecb55a8a0b5f18fc7d
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-04 19:35:39 +0200

    ex

---
+++ Arrays-Advance/07.Magic-Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = Console.ReadLine().Split(' ').
+               Select(int.Parse).ToArray();
+
+            int givenNum = int.Parse(Console.ReadLine());
+            bool isSpecial = false;
+            for (int i = 0; i < num.Length; i++)
+            {
+                int firstDigit = 0;
+                firstDigit = num[i];
+
+                for (int j = i + 1; j < num.Length; j++)
+                {
+                    int secondDigit = 0;
+                    secondDigit = num[j];
+                    if (firstDigit + secondDigit == givenNum)
+                    {
+                        Console.WriteLine($"{firstDigit} {secondDigit}");
+
+                    }
+
+                }
+
+            }
+        }
+    }
+}
