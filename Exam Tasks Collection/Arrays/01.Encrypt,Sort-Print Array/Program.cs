78d8e38aab14818a4c55efb22602991545fbe252
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-06 10:22:29 +0200

    ex

---
+++ Exam Tasks Collection/Arrays/01.Encrypt,Sort-Print Array/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.Encrypt_Sort_Print_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numNames = int.Parse(Console.ReadLine());
+
+            char[] vowel = new char[] { 'a', 'A', 'e', 'E', 'i', 'I', 'o', 'O', 'u', 'U' };
+
+
+            int[] output = new int[numNames];
+
+            for (int i = 0; i <= numNames - 1; i++)
+            {
+                string input = Console.ReadLine();
+                int consonantSum = 0;
+                int sumAplhabet = 0;
+                int vowelsSum = 0;
+
+                foreach (var item in input)
+                {
+                    if (vowel.Contains(item))
+                    {
+                        vowelsSum += (int)item * input.Length;
+                    }
+                    else
+                    {
+                        consonantSum += (int)item / input.Length;
+                    }
+
+                }
+                sumAplhabet += vowelsSum + consonantSum;
+
+                output[i] += sumAplhabet;
+            }
+
+            Array.Sort(output);
+
+            foreach (var item in output)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
