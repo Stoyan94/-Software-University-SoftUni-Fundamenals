8e03221bf769c83cbadd9a269db0c8aeb718e0d5
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-06 10:36:44 +0200

    ex

---
+++ Methods/02.Grades/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _02.Grades
+{
+    internal class Program
+    {
+        private static bool x;
+
+        static void Main(string[] args)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            PrintGrade(grade);
+
+        }
+
+        static void PrintGrade(double grade)
+        {
+            if (grade <= 2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (grade <= 3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if (grade <= 4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (grade <= 5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else
+            {
+                Console.WriteLine("Excellent");
+            }
+
+        }
+
+    }
+}
