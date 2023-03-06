e4e38f2d69f50a7f59cd3b204eb04c13ff47f62a
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-06 10:40:21 +0200

    ex

---
+++ Methods/04.Print-Triangle/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04.Print_Triangle
+{
+    internal class Program
+    {
+        private static bool x;
+
+        static void Main(string[] args)
+        {
+            int height = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < height; i++)
+            {
+                PrinUpLine(1, i);
+
+            }
+            for (int j = height; j >= 1; j--)
+            {
+                PrinUpLine(1, j);
+            }
+
+        }
+
+        static void PrinUpLine(int start, int end)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                Console.Write($"{i} ");
+
+            }
+            Console.WriteLine();
+        }
+    }
+}
