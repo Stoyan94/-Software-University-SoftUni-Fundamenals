5224da29528b4a205aa799d48cf1678c086db032
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-10 12:06:33 +0200

    ex

---
+++ Methods/06.Calculate-Rectangle-Area/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _06.Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        private static bool x;
+
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = PrintRectangle(width, height);
+            Console.WriteLine(area);
+        }
+
+        static double PrintRectangle(double width, double height)
+        {
+            return width * height;
+        }
+    }
+}
