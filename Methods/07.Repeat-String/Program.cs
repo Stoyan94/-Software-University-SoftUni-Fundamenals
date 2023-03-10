55b0e41c17493bf7d53fc7d6b525d63419c590ca
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-10 12:07:39 +0200

    ex

---
+++ Methods/07.Repeat-String/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _07.Repeat_String
+{
+    internal class Program
+    {
+        private static bool x;
+
+        static void Main(string[] args)
+        {
+            string repeat = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            string final = PrintRepeat(repeat, n);
+            Console.WriteLine(final);
+        }
+
+        private static string PrintRepeat(string repeat, int n)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < n; i++)
+            {
+                result += repeat;
+            }
+            return result;
+        }
+    }
+}
