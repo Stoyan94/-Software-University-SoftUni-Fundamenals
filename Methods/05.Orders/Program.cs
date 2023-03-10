f99d9b03c0bbd7d76412adaad3b3924edbd0167a
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-10 12:05:13 +0200

    ex

---
+++ Methods/05.Orders/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _05.Orders
+{
+    internal class Program
+    {
+        private static bool x;
+
+        static void Main(string[] args)
+        {
+            string order = Console.ReadLine();
+            double orderPrice = double.Parse(Console.ReadLine());
+            PriceOrder(order, orderPrice);
+        }
+
+        static void PriceOrder(string order, double price)
+        {
+            if (order == "coffee")
+            {
+                price *= 1.50;
+            }
+            else if (order == "water")
+            {
+                price *= 1.00;
+            }
+            else if (order == "coke")
+            {
+                price *= 1.40;
+            }
+            else if (order == "snacks")
+            {
+                price *= 2.00;
+            }
+            Console.WriteLine($"{price:f2}");
+        }
+    }
+}
