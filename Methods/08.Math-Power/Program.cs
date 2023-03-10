1bb987223bc1fcc63a3bb972e1198e001bee2179
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-10 12:08:44 +0200

    ex

---
+++ Methods/08.Math-Power/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _08.Math_Power
+{
+    internal class Program
+    {
+        private static bool x;
+
+        static void Main(string[] args)
+        {
+            double baseNum = double.Parse(Console.ReadLine());
+            double powerNum = double.Parse(Console.ReadLine());
+            double powerdNum = PrintRepeat(baseNum, powerNum);
+            Console.WriteLine(powerdNum);
+        }
+
+        static double PrintRepeat(double baseNum, double PowerNUm)
+        {
+            return Math.Pow(baseNum, PowerNUm);
+
+        }
+    }
+}
