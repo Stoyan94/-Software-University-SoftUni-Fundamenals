6c05e34b0f466091f37390149a72ff7ca3793beb
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-11 10:55:17 +0200

    ex

---
+++ Methods-Advance/02.Vowels-Count/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Vowels_Count
+{
+    internal class Program
+    {
+        private static bool x;
+
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            Console.WriteLine(VowelsCoutn(word));
+        }
+
+        static int VowelsCoutn(string countVwoels)
+        {
+            int vowelsCount = 0;
+            char[] vowelsS = new char[] { 'a', 'e', 'i', 'o', 'u' };
+
+            foreach (char ch in countVwoels.ToLower())
+            {
+                if (vowelsS.Contains(ch))
+                {
+                    vowelsCount++;
+                }
+
+            }
+
+            return vowelsCount;
+        }
+    }
+}
