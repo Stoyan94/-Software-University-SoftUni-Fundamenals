7327a2de36b661f896764eafa4fb8cc0d3e70f8d
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-11 10:53:25 +0200

    ex

---
+++ Methods-Advance/01.Smallest-of-Three-nums/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _01.Smallest_of_Three_nums
+{
+    internal class Program
+    {
+        private static bool x;
+
+        static void Main(string[] args)
+        {
+            int numOne = int.Parse(Console.ReadLine());
+            int numTwo = int.Parse(Console.ReadLine());
+            int numThree = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine(ReturnkSmalletsNum(numOne, numTwo, numThree));
+
+        }
+
+
+        static int ReturnkSmalletsNum(int one, int tow, int three)
+        {
+            int smallestNum = int.MaxValue;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                if (three < smallestNum)
+                {
+                    smallestNum = three;
+                }
+                if (tow < smallestNum)
+                {
+                    smallestNum = tow;
+                }
+                if (one < smallestNum)
+                {
+                    smallestNum = one;
+                }
+
+            }
+
+            return smallestNum;
+        }
+
+
+    }
+}
