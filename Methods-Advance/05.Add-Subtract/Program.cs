e4eadce6bebe1903cdeb2394508140d789c6d548
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-14 16:30:59 +0200

    ex

---
+++ Methods-Advance/05.Add-Subtract/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05.Add_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int seckondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+            int sumNumbers = SumFirstAndSeckond(firstNum, seckondNum);
+            int substratedNum = SubstractSumDigits(sumNumbers, thirdNum);
+
+            Console.WriteLine(substratedNum);
+
+        }
+
+        static int SumFirstAndSeckond(int firstNum, int seckondNum)
+        {
+            return firstNum + seckondNum;
+        }
+
+        static int SubstractSumDigits(int sumNums, int substratNum)
+        {
+            return sumNums - substratNum;
+
+        }
+    }
+}
