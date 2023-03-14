4817bba364321af8fc64d823656b2f34f7562e80
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-14 16:32:52 +0200

    ex

---
+++ Methods-Advance/06.Middle-Characters/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _06.Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            char mid = char.MinValue;
+            char midEven = char.MinValue;
+            string midLetter = string.Empty;
+            if (word.Length % 2 != 0)
+            {
+                for (int i = 0; i < word.Length / 2 + 1; i++)
+                {
+                    mid = word[i];
+
+                }
+                midLetter = mid.ToString();
+                Console.WriteLine(midLetter);
+            }
+            else
+            {
+                for (int i = 0; i < word.Length / 2; i++)
+                {
+                    mid = word[i];
+
+                    if (i == word.Length / 2 - 1)
+                    {
+                        midEven = word[i + 1];
+                    }
+                }
+
+                midLetter = mid.ToString() + midEven.ToString();
+
+                Console.WriteLine(midLetter);
+            }
+
+
+        }
+    }
+}
