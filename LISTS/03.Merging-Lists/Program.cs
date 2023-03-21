405a6fc307124b88fabce9c42ad1a9ff8457d473
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-21 13:36:26 +0200

    ex

---
+++ LISTS/03.Merging-Lists/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Merging_Lists
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> secondList = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            int longestList = Math.Max(firstList.Count, secondList.Count);
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < longestList; i++)
+            {
+                if (i < firstList.Count)
+                {
+                    result.Add(firstList[i]);
+
+                }
+                if (i < secondList.Count)
+                {
+                    result.Add(secondList[i]);
+
+                }
+
+
+            }
+
+            Console.WriteLine(String.Join(" ", result));
+        }
+    }
+}
