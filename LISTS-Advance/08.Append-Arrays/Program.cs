de2dc0ec1ebf1d6b29ba844349ffe19527a70855
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-29 13:47:46 +0300

    ex

---
+++ LISTS-Advance/08.Append-Arrays/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> items = Console.ReadLine().Split('|', StringSplitOptions.RemoveEmptyEntries).ToList();
+
+
+            for (int i = items.Count - 1; i >= 0; i--)
+            {
+                List<string> reversedList = items[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+
+
+                for (int j = 0; j < reversedList.Count; j++)
+                {
+
+                    Console.Write($"{reversedList[j]} ");
+
+
+                }
+
+            }
+        }
+    }
+}
