a38dcbd6f73aa5208f331652c4841299cb839aee
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-30 08:47:34 +0300

    ex

---
+++ Objects and Classes/02.Big-Factorial/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Numerics;
+
+namespace _02.Big_Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger number = int.Parse(Console.ReadLine());
+
+            BigInteger Bigfactorial = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                Bigfactorial = Bigfactorial * i;
+            }
+
+            Console.WriteLine(Bigfactorial);
+        }
+    }
+}
