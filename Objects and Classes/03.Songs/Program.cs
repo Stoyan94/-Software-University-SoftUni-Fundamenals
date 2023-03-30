361938854e354430e2001c71d9bc9946c13354a4
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-30 08:50:29 +0300

    ex

---
+++ Objects and Classes/03.Songs/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03.Songs
+{
+    class Song
+    {
+        public string Name { get; set; }
+
+        public string TypeList { get; set; }
+
+        public string Time { get; set; }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSongs = int.Parse(Console.ReadLine());
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < numberOfSongs; i++)
+            {
+                string[] tokens = Console.ReadLine()
+                    .Split('_', StringSplitOptions.RemoveEmptyEntries);
+
+                string typeList = tokens[0];
+                string name = tokens[1];
+                string time = tokens[2];
+
+                Song mewSong = new Song()
+                {
+                    Name = name,
+                    TypeList = typeList,
+                    Time = time
+                };
+                songs.Add(mewSong);
+            }
+
+            string typeListToSearch = Console.ReadLine();
+
+            if (typeListToSearch == "all")
+            {
+                foreach (var song in songs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+            else
+            {
+                List<Song> filtredSongs = songs.FindAll
+                    (song => song.TypeList == typeListToSearch);
+
+                foreach (var song in filtredSongs)
+                {
+                    Console.WriteLine(song.Name);
+                }
+            }
+        }
+
+    }
+}
