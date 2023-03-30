995bdf89fdb61095a84382bb2b71d2d1fbb4d898
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-30 08:45:21 +0300

    ex

---
+++ Objects and Classes/01.Randomize-Words/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _01.Randomize_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Random random = new Random();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomIdex = random.Next(0, words.Length);
+
+                string currentWord = words[i];
+                words[i] = words[randomIdex];
+                words[randomIdex] = currentWord;
+
+            }
+
+            foreach (var randomIndex in words)
+            {
+                Console.WriteLine(randomIndex);
+            }
+        }
+    }
+}
