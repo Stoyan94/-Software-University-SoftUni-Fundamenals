d35be98a3f34b7bbb4d9bced1bd76ec5b786aa8f
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-03-30 08:53:04 +0300

    ex

---
+++ Objects and Classes/04.Students/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.Students
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public int Age { get; set; }
+
+        public string HomeTown { get; set; }
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command;
+            List<Student> students = new List<Student>();
+
+            while ((command = Console.ReadLine()) != "end")
+            {
+                string[] splitParam = command.
+                    Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string firstName = splitParam[0];
+                string lastName = splitParam[1];
+                int age = int.Parse(splitParam[2]);
+                string homeTown = splitParam[3];
+
+                Student newStudent = new Student()
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Age = age,
+                    HomeTown = homeTown
+                };
+                students.Add(newStudent);
+            }
+
+            string homeTownToSearch = Console.ReadLine();
+
+            List<Student> filteredStudents = students.FindAll(students => students.HomeTown == homeTownToSearch);
+
+            foreach (var student in filteredStudents)
+            {
+                Console.WriteLine($"{ student.FirstName} { student.LastName} is {student.Age } years old.");
+            }
+        }
+
+    }
+}
