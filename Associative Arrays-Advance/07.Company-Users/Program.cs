c6fdfa7ebdb6cfe25b4aa5112d2c20f5937faa4f
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-06 11:54:28 +0300

    ex

---
+++ Associative Arrays-Advance/07.Company-Users/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.Company_Users
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companysInfo = new Dictionary<string, List<string>>();
+
+            string companyNameAndUser;
+
+            while ((companyNameAndUser = Console.ReadLine()) != "End")
+            {
+                string[] argsCmUser = companyNameAndUser.Split(" -> ",
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                string companyName = argsCmUser[0];
+                string userId = argsCmUser[1];
+
+                if (!companysInfo.ContainsKey(companyName))
+                {
+                    companysInfo[companyName] = new List<string>();
+                }
+                if (companysInfo[companyName].Contains(userId))
+                {
+                    continue;
+                }
+                companysInfo[companyName].Add(userId);
+            }
+
+            foreach (var compName in companysInfo)
+            {
+                Console.WriteLine($"{compName.Key}");
+
+                foreach (var userId in compName.Value)
+                {
+                    Console.WriteLine($"-- {userId}");
+                }
+            }
+        }
+    }
+}
