635b175a8e24276673db447ebab9696556db3660
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-06 11:37:15 +0300

    ex

---
+++ Associative Arrays-Advance/01.Count-Chars-in-String/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Count_Chars_in_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            Dictionary<char, int> charCount = new Dictionary<char, int>();
+
+            foreach (var item in text.ToCharArray())
+            {
+                if (!charCount.ContainsKey(item))
+                {
+                    charCount[item] = 0;
+                }
+                charCount[item] += 1;
+            }
+
+            foreach (var item in charCount.Where(charCount => charCount.Key != ' '))
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
