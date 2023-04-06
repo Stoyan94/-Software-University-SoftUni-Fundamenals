5f929156c5060b94778538ee424949de37494ef1
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-06 11:26:14 +0300

    ex

---
+++ Associative Arrays/04.Word-Filter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.Word_Filter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] result = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(r => r.Length % 2 == 0).ToArray();
+
+            foreach (string s in result)
+            {
+                Console.WriteLine(string.Join(' ', s));
+
+            }
+
+        }
+    }
+}
