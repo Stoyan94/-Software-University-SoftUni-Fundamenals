99a2ca569c5f48095a3637183ad92825613beb6b
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-06 11:50:20 +0300

    ex

---
+++ Associative Arrays-Advance/05.Courses/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.Courses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> courseInfoSave = new Dictionary<string, List<string>>();
+
+            string courseInfoProces;
+
+            while ((courseInfoProces = Console.ReadLine()) != "end")
+            {
+                string[] argsCursuInfo = courseInfoProces.Split(" : ",
+                    StringSplitOptions.RemoveEmptyEntries);
+
+                string courseName = argsCursuInfo[0];
+                string studentName = argsCursuInfo[1];
+
+                if (!courseInfoSave.ContainsKey(courseName))
+                {
+                    courseInfoSave[courseName] = new List<string>();
+                }
+                courseInfoSave[courseName].Add(studentName);
+
+
+            }
+
+            foreach (var courseName in courseInfoSave)
+            {
+                Console.WriteLine($"{courseName.Key}: {courseName.Value.Count}");
+
+                foreach (var studentName in courseName.Value)
+                {
+                    Console.WriteLine($"-- {string.Join(" ", studentName)}");
+                }
+            }
+        }
+    }
+}
