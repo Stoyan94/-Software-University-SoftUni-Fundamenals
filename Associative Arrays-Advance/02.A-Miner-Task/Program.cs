764270d9f10151dd6506d8addef410f6eb1e55a1
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-06 11:40:19 +0300

    ex

---
+++ Associative Arrays-Advance/02.A-Miner-Task/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.A_Miner_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> oreTypes = new Dictionary<string, int>();
+
+            string ores;
+
+            while ((ores = Console.ReadLine()) != "stop")
+            {
+                int oreAmmount = int.Parse(Console.ReadLine());
+
+                if (oreTypes.ContainsKey(ores))
+                {
+                    oreTypes[ores] += oreAmmount;
+                    continue;
+                }
+
+                oreTypes.Add(ores, oreAmmount);
+            }
+
+            foreach (var ore in oreTypes)
+            {
+                Console.WriteLine($"{ore.Key} -> {ore.Value}");
+            }
+        }
+    }
+}
