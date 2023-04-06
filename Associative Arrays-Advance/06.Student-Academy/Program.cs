4c1db1751c1f6ae542a2ef3824c56bf46771f4e6
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-06 11:52:13 +0300

    ex

---
+++ Associative Arrays-Advance/06.Student-Academy/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.Student_Academy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<double>> studentGrades = new Dictionary<string, List<double>>();
+
+            int studentsCount = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= studentsCount; i++)
+            {
+                string studentName = Console.ReadLine();
+                double studentGrade = double.Parse(Console.ReadLine());
+
+                if (!studentGrades.ContainsKey(studentName))
+                {
+                    studentGrades[studentName] = new List<double>();
+                }
+                studentGrades[studentName].Add(studentGrade);
+
+
+            }
+
+            foreach (var student in studentGrades)
+            {
+                if (student.Value.Average() >= 4.50)
+                {
+                    Console.WriteLine($"{student.Key} -> {student.Value.Average():F2}");
+                }
+            }
+        }
+    }
+}
