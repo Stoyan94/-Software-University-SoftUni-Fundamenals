a81cabf42e4183f18b5f2109c48ab7bd5ee305bc
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-12 11:22:43 +0300

    ex

---
+++ Text Processing/05.Digits-Letters-Chars/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace _05.Digits_Letters_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder letter = new StringBuilder();
+            StringBuilder numbers = new StringBuilder();
+            StringBuilder charecters = new StringBuilder();
+            string text = Console.ReadLine();
+
+            foreach (var item in text)
+            {
+                if (char.IsLetter(item))
+                {
+                    letter.Append(item);
+                }
+                else if (char.IsDigit(item))
+                {
+                    numbers.Append(item);
+                }
+                else
+                {
+                    charecters.Append(item);
+                }
+            }
+            Console.WriteLine(numbers);
+            Console.WriteLine(letter);
+            Console.WriteLine(charecters);
+        }
+    }
+}
