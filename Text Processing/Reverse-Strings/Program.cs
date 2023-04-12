8a1e0d5ee5fefa60d7a66b9c3d09832ddc473166
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-12 11:13:11 +0300

    ex

---
+++ Text Processing/Reverse-Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace Reverse_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word;
+
+
+            while ((word = Console.ReadLine()) != "end")
+            {
+                char[] revers = word.Reverse().ToArray();
+                StringBuilder sb = new StringBuilder();
+
+                sb.Append(revers);
+
+                Console.WriteLine($"{word} = {sb}");
+            }
+        }
+    }
+}
