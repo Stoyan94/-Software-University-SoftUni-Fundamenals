10845fdc38babe75a093032321c55baa930ae82a
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-12 11:15:23 +0300

    ex

---
+++ Text Processing/02.Repeat-Strings/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _02.Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] reprint = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder result = new StringBuilder();
+            foreach (var word in reprint)
+            {
+                int currWord = word.Count();
+
+                for (int i = 0; i < currWord; i++)
+                {
+                    result.Append(word);
+                }
+
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
