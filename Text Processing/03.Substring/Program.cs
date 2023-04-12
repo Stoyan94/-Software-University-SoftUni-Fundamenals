a31aa7a4933005d2701b676d3dde997da3489546
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-12 11:16:56 +0300

    ex

---
+++ Text Processing/03.Substring/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _03.Substring
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string removedWord = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            while (text.Contains(removedWord))
+            {
+                int indexOfWordToRemove = text.IndexOf(removedWord);
+
+                text = text.Remove(indexOfWordToRemove, removedWord.Length);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
