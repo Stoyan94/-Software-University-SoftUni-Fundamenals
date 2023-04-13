c7213495b75d8c003d05ffd34f7c41c606efcd43
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-13 12:04:44 +0300

    ex

---
+++ Text Processing-Advance/02.Character-Multiplier/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _02.Character_Multiplier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            char[] char1 = input[0].ToCharArray();
+            char[] char2 = input[1].ToCharArray();
+
+            int totalSum = 0;
+            for (int i = 0; i < Math.Max(char1.Length, char2.Length); i++)
+            {
+                if (i < char1.Length && i < char2.Length)
+                {
+                    totalSum += char1[i] * char2[i];
+                }
+                else if (i < char1.Length && i >= char2.Length)
+                {
+                    totalSum += char1[i];
+                }
+                else if (i >= char1.Length && i < char2.Length)
+                {
+                    totalSum += char2[i];
+                }
+            }
+            Console.WriteLine(totalSum);
+        }
+    }
+}
