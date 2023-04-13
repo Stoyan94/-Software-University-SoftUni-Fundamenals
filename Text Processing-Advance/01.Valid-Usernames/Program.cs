d6f99ec6e7202b621653a12c657e37acbe355899
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-13 12:01:57 +0300

    ex

---
+++ Text Processing-Advance/01.Valid-Usernames/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace _01.Valid_Usernames
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] searchUsername = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            bool isValidUser = true;
+            char[] containtThis = new char[] { '_', '-' };
+
+            foreach (string username in searchUsername)
+            {
+                if (username.Length >= 3 && username.Length <= 16)
+                {
+                    foreach (var symbol in username)
+                    {
+                        if (char.IsLetterOrDigit(symbol) || symbol == '_' || symbol == '-')
+                        {
+                            isValidUser = true;
+                        }
+                        else
+                        {
+                            isValidUser = false;
+                            break;
+                        }
+                    }
+                    if (isValidUser)
+                    {
+                        Console.WriteLine(username);
+                    }
+                }
+
+            }
+        }
+    }
+}
