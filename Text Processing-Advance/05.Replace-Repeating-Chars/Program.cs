d4e915192b907cea3b5b9089185d8ff2fefcb984
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-14 13:58:08 +0300

    ex

---
+++ Text Processing-Advance/05.Replace-Repeating-Chars/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine().ToList();
+
+            for (int i = 0; i < str.Count - 1; i++)
+            {
+                char curr = str[i];
+                char next = str[i + 1];
+                if (curr == next)
+                {
+                    str.RemoveAt(i);                  
+                    i--;
+                }
+            }
+
+            Console.WriteLine(string.Join("", str));
+        }
+    }
+}
