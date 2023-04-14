80a8d8af3652a92da03fe30a5ff3af63862c14aa
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-14 14:05:52 +0300

    ex

---
+++ Exam Tasks Collection/Text Processing/02.Letters-Change-Numbers/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Letters_Change_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            decimal sum = 0;
+
+            foreach (string word in words)
+            {
+                sum += CalculateSingleWordSum(word);
+            }
+            Console.WriteLine($"{sum:F2}");
+        }
+
+        static decimal CalculateSingleWordSum(string word)
+        {
+            decimal sum = 0;
+
+            char firstLetter = word[0];
+            char lastLetter = word[word.Length - 1];
+            int num = int.Parse(word.Substring(1, word.Length - 2));
+
+            int firstLetterPosition = GetAlphabeticalPositionOfCharecter(firstLetter);
+            int lastLetterPosition = GetAlphabeticalPositionOfCharecter(lastLetter);
+
+            if (char.IsUpper(firstLetter))
+            {
+                sum = (decimal)num / firstLetterPosition;
+            }
+            else if (char.IsLower(firstLetter))
+            {
+                sum = (decimal)num * firstLetterPosition;
+            }
+
+            if (char.IsUpper(lastLetter))
+            {
+                sum -= lastLetterPosition;
+            }
+            else if (char.IsLower(lastLetter))
+            {
+                sum += lastLetterPosition;
+            }
+            return sum;
+        }
+
+        static int GetAlphabeticalPositionOfCharecter(char ch)
+        {
+            if (!char.IsLetter(ch))
+            {
+                return -1;
+            }
+
+            char chCI = char.ToLowerInvariant(ch);
+
+            return (int)chCI - 96;
+        }
+    }
+}
