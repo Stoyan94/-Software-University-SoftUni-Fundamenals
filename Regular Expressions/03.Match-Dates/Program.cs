7f357964b5eba5a6247d5303cbec10b7ff90a8e6
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-15 13:36:24 +0300

    ex

---
+++ Regular Expressions/03.Match-Dates/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _03.Match_Dates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\b(\d{2})(\/|-|\.)([A-Z][a-z]{2})\2(\d{4})\b";
+
+            string input = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match item in matches)
+            {
+                string day = item.Groups[1].Value;
+                string month = item.Groups[3].Value;
+                string year = item.Groups[4].Value;
+
+                Console.WriteLine($" Day: {day}, Month: {month}, Year: {year}");
+            }
+        }
+    }
+}
