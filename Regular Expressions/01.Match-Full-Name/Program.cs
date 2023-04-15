c7c33d369df9959969fd0e4ab4d17fc64275f43b
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-15 13:32:03 +0300

    ex

---
+++ Regular Expressions/01.Match-Full-Name/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01.Match_Full_Name
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pater = @"\b([A-Z][a-z]{1,}) ([A-Z][a-z]{1,})\b";
+
+            string text = Console.ReadLine();
+
+            MatchCollection matches = Regex.Matches(text, pater);
+
+            foreach (Match match in matches)
+            {
+                Console.Write(match.Value + " ");
+            }
+        }
+    }
+}
