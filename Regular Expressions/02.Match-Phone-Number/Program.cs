aa9087c7ddb394ce9e0a38d922b2cced55e2a1b5
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-04-15 13:34:58 +0300

    ex

---
+++ Regular Expressions/02.Match-Phone-Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace _02.Match_Phone_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"\+359( |-)2\1\d{3}\1\d{4}\b";
+
+            string input = Console.ReadLine();
+
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            List<string> result = new List<string>();
+
+            foreach (Match match in matches)
+            {
+                result.Add(match.Value);
+            }
+
+            Console.WriteLine(String.Join(", ", result));
+        }
+    }
+}
