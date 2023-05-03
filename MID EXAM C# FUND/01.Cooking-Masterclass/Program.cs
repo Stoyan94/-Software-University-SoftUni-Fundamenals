9b2c75d7233dd32ca1e0604c71bf28fb94894ae5
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-05-03 11:38:01 +0300

    ex

---
+++ MID EXAM C# FUND/01.Cooking-Masterclass/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _01.Cooking_Masterclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double budget = double.Parse(Console.ReadLine());
+
+            int students = int.Parse(Console.ReadLine());
+
+            double priceOneFlour = double.Parse(Console.ReadLine());
+            double priceOneEgg = double.Parse(Console.ReadLine());
+            double priceOneAproon = double.Parse(Console.ReadLine());
+
+            double totalSum = priceOneFlour * (students - (students / 5));
+            totalSum += (priceOneEgg * 10) * students;
+            double priceAproons = priceOneAproon * Math.Ceiling(students * 1.2);
+
+            totalSum += priceAproons;
+
+            budget -= totalSum;
+
+        }
+    }
+}
