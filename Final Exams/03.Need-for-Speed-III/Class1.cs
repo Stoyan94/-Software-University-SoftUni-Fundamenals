0a39c9c08c477c481b1168eda667f36167fe5d49
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2023-05-07 19:09:31 +0300

    ex

---
+++ Final Exams/03.Need-for-Speed-III/Class1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.Need_for_Speed_III
+{
+    public class Cars
+    {
+        private string carModel;
+        private int mileage;
+        private int fuel;
+
+        public Cars(string carName,int carMilage, int carFuel)
+        {
+            CarModel = carName;
+            Mileage = carMilage;
+            Fuel = carFuel;
+            
+        }
+
+        public string CarModel { get { return this.carModel; }  set { carModel = value; } }
+
+        public int Mileage { get { return this.mileage; } set { mileage = value; } }
+
+        public int Fuel { get { return this.fuel; } set { fuel = value; } }
+
+       public void Drive(int milage,int fuel,string currCar,List<Cars> allCars)
+        {
+            int checkMilage=  this.Mileage + milage;
+            
+            if (checkMilage>100000)
+            {
+                Cars car = allCars.Where(x => x.CarModel==currCar).FirstOrDefault();
+                allCars.Remove(car);
+                Console.WriteLine($"Time to sell the {currCar}!");
+                return;
+            }
+            if (this.fuel-fuel<0)
+            {
+                Console.WriteLine("Not enough fuel to make that ride");
+                return;
+            }
+            this.Mileage += milage;
+            this.Fuel-=fuel;
+
+            Console.WriteLine($"{currCar} driven for {milage} kilometers. {fuel} liters of fuel consumed.");
+            
+        }
+
+        public void Reful(int refuel)
+        {
+            if (this.Fuel+refuel<75)
+            {
+                this.Fuel += refuel;
+            }
+            else
+            {
+                int fuelToAdd = 75-this.fuel;
+                this.Fuel += fuelToAdd;
+            }
+
+            Console.WriteLine($"{this.carModel} refueled with {this.fuel} liters");
+        }
+       
+        public void Revert(int km)
+        {
+            if (this.mileage-km<10000)
+            {
+                this.mileage= 10000;                
+            }
+            else
+            {
+                this.mileage-= km;
+                Console.WriteLine($"{this.carModel} mileage decreased by {km} kilometers");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.carModel} -> Milage: {this.mileage} kms, Fuel in the tank: {this.Fuel} it.";
+        }
+    }
+
+  
+}
---
+++ Final Exams/03.Need-for-Speed-III/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _03.Need_for_Speed_III
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {                           
+            List<Cars> cars = new List<Cars>();
+
+            int numCars = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numCars; i++)
+            {
+                string[] carInfo = Console.ReadLine().Split('|');
+
+                string carModel = carInfo[0];
+                int milage = int.Parse(carInfo[1]);
+                int fuel = int.Parse(carInfo[2]);
+
+                Cars car = new Cars(carModel,milage,fuel);
+
+                cars.Add(car);
+            }
+
+            string input;
+
+            while ((input=Console.ReadLine())!="Stop")
+            {
+                string [] commandCar = input.Split(" :",StringSplitOptions.TrimEntries);
+
+                string command = commandCar[0];
+                string carModel = commandCar[1];
+                int tryMilage = int.Parse(commandCar[2]);
+
+                Cars currCar = cars.FirstOrDefault(x => x.CarModel == carModel);
+
+                if (command == "Drive")
+                {
+                    int fuelDrive = int.Parse(commandCar[3]);                   
+                    currCar.Drive(tryMilage, fuelDrive,carModel,cars);            
+                }
+                else if (command == "Refuel")
+                {
+                    int refuel = int.Parse(commandCar[2]);
+                    currCar.Reful(refuel);
+                }
+                else if (command == "Revert")
+                {
+                    int revertKm = int.Parse(commandCar[2]);
+                    currCar.Revert(revertKm);
+                }
+            }
+
+            foreach (var car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+
+       
+    }
+}
