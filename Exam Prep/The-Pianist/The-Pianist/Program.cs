6709064889a83fdc861142a8a4d9332524c339dd
Stoqn <podlobg@gmail.com>
2024-03-01 15:34:53 +0200

    up

--- Exam Prep/The-Pianist/The-Pianist/Program.cs
+++ Exam Prep/The-Pianist/The-Pianist/Program.cs
@@ -32,13 +32,12 @@ while ((commands = Console.ReadLine()) != "Stop")
     string[] cmdArgs = commands.Split("|").ToArray();
 
     string command = cmdArgs[0];
+    string songName = cmdArgs[1];
 
     if (command == "Add")
-    {
-        string songName = cmdArgs[0];
-        string creator = cmdArgs[1];
-        string key = cmdArgs[2];
-
+    {      
+        string creator = cmdArgs[2];
+        string key = cmdArgs[3];
 
         if (dataTable.ContainsKey(songName))
         {
@@ -51,15 +50,40 @@ while ((commands = Console.ReadLine()) != "Stop")
             {"composer", creator},
             {"key", key},
         });
+            Console.WriteLine($"{songName} by {creator} in {key} added to the collection!");
         }
-    }
-    
+    }    
     else if (command == "Remove")
     {
+        if (dataTable.ContainsKey(songName))
+        {
+            dataTable.Remove(songName);
+            Console.WriteLine($"Successfully removed {songName}!");
 
+        }
+        else
+        {
+            Console.WriteLine($"Invalid operation! {songName} does not exist in the collection.");
+        }
     } 
     else if (command == "ChangeKey")
     {
+        string newKey = cmdArgs[2];
 
+        if (dataTable.ContainsKey(songName))
+        {
+            dataTable[songName]["key"] = newKey;
+            Console.WriteLine($"Changed the key of {songName} to {newKey}!");
+        }
+        else 
+        {
+            Console.WriteLine($"Invalid operation! {songName} does not exist in the collection.");
+        }
     }
+}
+
+
+foreach (var item in dataTable)
+{
+    Console.WriteLine($"{item.Key} -> Composer: {item.Value["composer"]}, Key: {item.Value["key"]}");
 }
