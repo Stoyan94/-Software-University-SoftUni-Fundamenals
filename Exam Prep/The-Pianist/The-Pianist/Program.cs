6d732e3fc2aff5a4d36417b8e1a47eb89c20d165
Stoqn <podlobg@gmail.com>
2024-03-01 14:59:49 +0200

    up

---
+++ Exam Prep/The-Pianist/The-Pianist/Program.cs
@@ -0,0 +1,65 @@
+﻿
+
+int songsCount = int.Parse(Console.ReadLine());
+
+Dictionary<string, Dictionary<string, string>> dataTable = new Dictionary<string, Dictionary<string, string>>();
+
+for (int i = 0; i < songsCount; i++)
+{
+    string[] piecesInfo = Console.ReadLine().Split("|").ToArray();
+
+    string songName = piecesInfo[0];
+    string creator = piecesInfo[1];
+    string key = piecesInfo[2];
+
+
+    if (!dataTable.ContainsKey(songName))
+    {
+        dataTable.Add(songName, new Dictionary<string, string>()
+        {
+            {"composer", creator},
+            {"key", key},
+        });
+    }
+
+
+}
+
+string commands;
+
+while ((commands = Console.ReadLine()) != "Stop")
+{
+    string[] cmdArgs = commands.Split("|").ToArray();
+
+    string command = cmdArgs[0];
+
+    if (command == "Add")
+    {
+        string songName = cmdArgs[0];
+        string creator = cmdArgs[1];
+        string key = cmdArgs[2];
+
+
+        if (dataTable.ContainsKey(songName))
+        {
+            Console.WriteLine($"{songName} is already in the collection!");
+        }
+        else
+        {
+            dataTable.Add(songName, new Dictionary<string, string>()
+        {
+            {"composer", creator},
+            {"key", key},
+        });
+        }
+    }
+    
+    else if (command == "Remove")
+    {
+
+    } 
+    else if (command == "ChangeKey")
+    {
+
+    }
+}
