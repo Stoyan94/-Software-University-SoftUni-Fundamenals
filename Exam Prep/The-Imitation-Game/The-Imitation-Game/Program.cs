be3a84ad9658d5830bd6ff6ec7446c4a70f60da9
Stoqn <podlobg@gmail.com>
2024-03-01 13:38:34 +0200

    up

--- Exam Prep/The-Imitation-Game/The-Imitation-Game/Program.cs
+++ Exam Prep/The-Imitation-Game/The-Imitation-Game/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+//List<string> words = Console.ReadLine().Split().ToList();
+
+string word = Console.ReadLine();
+
+string commands;
+
+while ((commands = Console.ReadLine()) != "Decode")
+{
+    string[] cmdArs = commands.Split("|").ToArray();
+
+    string command = cmdArs[0];
+
+
+    if (command == "ChangeAll")
+    {
+        string letterToSwap = cmdArs[1];
+        string putThisLeeter = cmdArs[2];
+       
+        word = word.Replace(letterToSwap, putThisLeeter);
+    }
+    else if (command == "Insert")
+    {
+        int indexInsert = int.Parse(cmdArs[1]);
+        string letterInser = cmdArs[2];
+
+       word = word.Insert(indexInsert, letterInser);
+    } 
+    else if (command == "Move")
+    {
+        int countIndexMove = int.Parse(cmdArs[1]);
+
+        string moveThis = word.Substring(0, countIndexMove);
+
+        word = word.Remove(0, countIndexMove);
+
+        word = word.Insert(word.Length, moveThis);
+    }
+}
+
+Console.WriteLine($"The decrypted message is: {word}");
