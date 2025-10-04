4ba4f33f7b7176334e95a12e30d23467a85d8f33
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2025-10-04 15:05:37 +0300

    Update Program.cs

--- LISTS-Advance/01.List-Manipolation-Advance/Program.cs
+++ LISTS-Advance/01.List-Manipolation-Advance/Program.cs
@@ -5,109 +5,99 @@ using System.Linq;
 namespace _01.List_Manipolation_Advance
 {
     internal class Program
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<int> numbers = Console.ReadLine()
+            .Split(' ',StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+
+        int number = 0;
+        string operatorInput = string.Empty;
+
+        bool isChanged = false;
+
+        string commands;
+
+        while ((commands = Console.ReadLine()) != "end")
         {
-            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+            string[] commandArg = commands.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+            string command = commandArg[0];
 
-            bool isChange = false;
-            string command;
-
-            while ((command = Console.ReadLine()) != "end")
+            if (commandArg.Length > 1 && commandArg.Length <= 2)
             {
-
-                string[] token = command.Split();
-                string action = token[0];
-
-                if (action == "Add")
-                {
-                    int addToken = int.Parse(token[1]);
-                    numbers.Add(addToken);
-                    isChange = true;
-                }
-                else if (action == "Remove")
-                {
-                    int removeToken = int.Parse(token[1]);
-                    numbers.Remove(removeToken);
-                    isChange = true;
-
-                }
-                else if (action == "RemoveAt")
-                {
-                    int removeAtToken = int.Parse(token[1]);
-                    numbers.RemoveAt(removeAtToken);
-                    isChange = true;
-
-                }
-                else if (action == "Insert")
-                {
-                    int insertToke = int.Parse(token[1]);
-                    int indexInser = int.Parse(token[2]);
-                    numbers.Insert(indexInser, insertToke);
-                    isChange = true;
-
-                }
-                else if (action == "Contains")
-                {
-                    int containsToke = int.Parse(token[1]);
-                    if (numbers.Contains(containsToke))
-                    {
-                        Console.WriteLine("Yes");
-
-                    }
-                    else
-                    {
-                        Console.WriteLine("No such number");
-                    }
-
-                }
-                else if (action == "PrintEven")
-                {
-                    List<int> even = numbers.FindAll(x => x % 2 == 0);
-                    Console.WriteLine(String.Join(" ", even));
-                }
-                else if (action == "PrintOdd")
-                {
-                    List<int> odd = numbers.FindAll(x => x % 2 != 0);
-                    Console.WriteLine(String.Join(" ", odd));
-                }
-                else if (action == "GetSum")
-                {
-                    int sum = numbers.Sum();
-                    Console.WriteLine(String.Join(" ", sum));
-                }
-                else if (action == "Filter")
-                {
-                    string toke = token[1];
-                    int numberForComper = int.Parse(token[2]);
-                    List<int> result = new List<int>();
-                    switch (toke)
-                    {
-                        case "<":
-                            result = numbers.FindAll(x => x < numberForComper);
-                            break;
-                        case ">":
-                            result = numbers.FindAll(x => x > numberForComper);
-                            break;
-                        case ">=":
-                            result = numbers.FindAll(x => x >= numberForComper);
-                            break;
-                        case "<=":
-                            result = numbers.FindAll(x => x <= numberForComper);
-                            break;
-
-                    }
-                    Console.WriteLine(String.Join(" ", result));
-                }
-
-
+                number = int.Parse(commandArg[1]);
+            }
+            else if (commandArg.Length > 2)
+            {
+                operatorInput = commandArg[1];
+                number = int.Parse(commandArg[2]);
             }
 
-            if (isChange)
-            {
-                Console.WriteLine(String.Join(" ", numbers));
-            }
 
+            if (command == "Add")
+            {
+                numbers.Add(number);
+                isChanged = true;
+            }
+            else if (command == "Remove")
+            {
+                numbers.Remove(number);
+                isChanged = true;
+            }
+            else if (command == "RemoveAt")
+            {
+                numbers.RemoveAt(number);
+                isChanged = true;
+            }
+            else if (command == "Insert")
+            {
+                int index = int.Parse(commands.Split(' ')[2]);
+                numbers.Insert(index, number);
+                isChanged = true;
+            }
+            else if(command == "Contains")
+            {
+                var result = numbers.Contains(number) ? number.ToString() : "No such number";
+                Console.WriteLine(result);
+            }
+            else if (command == "PrintEven")
+            {
+                Console.WriteLine(string.Join(" ",numbers.Where(x => x % 2 == 0)));
+            }
+            else if (command == "PrintOdd")
+            {
+                Console.WriteLine(string.Join(" ", numbers.Where(x => x % 2 != 0)));
+            }
+            else if (command == "GetSum")
+            {
+                Console.WriteLine(numbers.Sum());
+            }
+            else if (command == "Filter")
+            {
+                switch (operatorInput)
+                {
+                    case ">":
+                        Console.WriteLine(string.Join(" ",numbers.Where(x => x > number))); 
+                        break;
+                    case "<":
+                        Console.WriteLine(string.Join(" ", numbers.Where(x => x < number)));
+                        break;
+                    case ">=":
+                        Console.WriteLine(string.Join(" ", numbers.Where(x => x >= number)));
+                        break;
+                    case "<=":
+                        Console.WriteLine(string.Join(" ", numbers.Where(x => x <= number)));
+                        break;
+                    
+                }
+            }
         }
+
+        if (isChanged)
+        {
+            Console.WriteLine(string.Join(" ", numbers));
+        }                
     }
+  }
 }
