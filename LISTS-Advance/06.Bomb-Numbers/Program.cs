b49426d21eab78cad97aa390cc8637ea4e2b0f3b
Stoyan <122618231+Stoyan94@users.noreply.github.com>
2025-10-05 13:06:31 +0300

    Update Program.cs

--- LISTS-Advance/06.Bomb-Numbers/Program.cs
+++ LISTS-Advance/06.Bomb-Numbers/Program.cs
@@ -4,65 +4,55 @@ using System.Linq;
 
 namespace _06.Bomb_Numbers
 {
-    internal class Program
+       internal class Program
     {
         static void Main(string[] args)
         {
-            List<int> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).
-               Select(int.Parse).ToList();
-
-            int[] bombInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).
-                Select(int.Parse).ToArray();
-
-            int bombNumber = bombInfo[0];
-            int bombPower = bombInfo[1];
-           
-
-            while (true)
+            // Четем списъка от числа от конзолата
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+    
+            // Четем бомбата и силата ѝ
+            int[] bombData = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+    
+            int bombNumber = bombData[0]; // самият бомбен номер
+            int bombPower = bombData[1];  // силата (колко числа вляво и вдясно се махат)
+    
+            // Докато в списъка има бомба
+            while (numbers.Contains(bombNumber))
             {
-
-                int indexfOfbomb = numbers.IndexOf(bombNumber);
-                if (indexfOfbomb == -1)
-                {
-                    break;
-                }
-                else
-                {
-                    int rightIndex = indexfOfbomb + bombPower;                                            
-
-                    for (int countBombard = indexfOfbomb; countBombard <= rightIndex; countBombard++)
-                    {
-                        if (indexfOfbomb >= numbers.Count)
-                        {
-                            break;
-                        }
-
-                        numbers.RemoveAt(indexfOfbomb); 
-
-                    }
-
-                    int leftIndex = indexfOfbomb - bombPower;
-                    if (leftIndex < 0) 
-                    {
-                        leftIndex = 0;
-                    }
-
-                    for (int leftBombard = leftIndex; leftBombard < indexfOfbomb; leftBombard++)
-                    {
-                        if (leftIndex >= numbers.Count)
-                        {
-                            break;
-                        }
-
-                        numbers.RemoveAt(leftIndex);
-
-                    }
-
-                }
-
+                // намираме индекса на първата бомба
+                int bombIndex = numbers.IndexOf(bombNumber);
+    
+    
+                // колко можем да махнем вляво (ако бомбата е близо до началото)
+                int indicesToRemoveLeft = Math.Min(bombPower, bombIndex);
+    
+    
+                // колко можем да махнем вдясно (ако бомбата е близо до края)
+                int indicesToRemoveRight = Math.Min(bombPower, numbers.Count - bombIndex - 1);
+    
+    
+                // изчисляваме от кой индекс започва махането
+                int startIndex = bombIndex - indicesToRemoveLeft;
+    
+    
+                // изчисляваме колко елемента общо да махнем
+                int count = indicesToRemoveLeft + indicesToRemoveRight + 1; // +1 за самата бомба
+    
+    
+                // махаме диапазона от списъка
+                numbers.RemoveRange(startIndex, count);
             }
+    
+            // Накрая печатаме сумата на останалите елементи
             int sum = numbers.Sum();
-            Console.WriteLine(String.Join(" ", sum));
+            Console.WriteLine(sum);
         }
     }
 }
